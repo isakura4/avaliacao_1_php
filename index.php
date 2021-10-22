668a2717764074678fd34c736f62bd8e4cceda17
Isadora★ ^_^ <90213408+isakura4@users.noreply.github.com>
2021-10-22 10:13:14 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+if(!isset($_SESSION["USUARIO"])){
+    //redirecionar para o login
+    header("Location: pagina_autenticacao.php");
+
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá! :D</title>
+</head>
+<body>
+    <h1>Seja bem vinde <?php echo $_SESSION["USUARIO"];?>! :D</h1>
+
+    <a href="pagina_autenticacao.php?acao=logoff">Sair</a>
+
+</body>
+</html>
